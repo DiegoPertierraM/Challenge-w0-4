8190bb22a23039c1344abfee0aa340ebee3e8956
DiegoPertierraM <diegopmenendez@gmail.com>
2024-02-15 17:57:50 +0100

    Added countVowels function

--- helpers.js
+++ helpers.js
@@ -4,10 +4,26 @@ export const cutString = (str) => {
 
 export const isPalindrome = (str) => {
   const middle = Math.trunc(str.length / 2);
-  const firstPart = str.slice(0, middle);
-  let secondPart = str.slice(middle + 1, str.length);
+  const firstPart = str.slice(0, middle).toLowerCase();
+  let secondPart = str.slice(middle + 1, str.length).toLowerCase();
   if (firstPart === secondPart.split('').reverse().join('')) {
     return true;
   }
   return false;
+};
+
+export const countVowels = (str) => {
+  let counter = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (
+      str[i].toLowerCase() === 'a' ||
+      str[i].toLowerCase() === 'e' ||
+      str[i].toLowerCase() === 'i' ||
+      str[i].toLowerCase() === 'o' ||
+      str[i].toLowerCase() === 'u'
+    ) {
+      counter++;
+    }
+  }
+  return counter;
 };
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-import { cutString, isPalindrome } from './helpers.js';
+import { cutString, isPalindrome, countVowels } from './helpers.js';
 
 console.log('Expected: amell', cutString('Camello'));
-console.log('Expected: false', isPalindrome('murciélago'));
-console.log('Expected: true', isPalindrome('sometemos'));
+console.log('Expected: false', isPalindrome('caravana'));
+console.log('Expected: true', isPalindrome('Sometemos'));
+console.log('Expected: 5', countVowels('muerciélago'));
